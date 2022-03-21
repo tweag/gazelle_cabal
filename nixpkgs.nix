ce769226d0a356aa71444107cf773e80fe5d5194
Facundo Domínguez <facundo.dominguez@tweag.io>
2022-03-21 18:01:10 -0300

    Try bazel 5

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,1 +1,1 @@
-import (fetchTarball "https://github.com/nixos/nixpkgs/archive/89f196fe781c53cb50fef61d3063fa5e8d61b6e5.tar.gz")
+import (fetchTarball "https://github.com/nixos/nixpkgs/archive/9bc841fec1c0e8b9772afa29f934d2c7ce57da8e.tar.gz")
