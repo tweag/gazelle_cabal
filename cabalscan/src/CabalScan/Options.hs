29b4700d3a572b1f2ee9d96d5e9d92c8b3fcd5e6
kczulko <karol.czulkowski@protonmail.com>
2022-03-31 09:19:21 +0200

    Use GetOpt for parsing cli parameters.

--- cabalscan/src/CabalScan/Options.hs
+++ cabalscan/src/CabalScan/Options.hs
@@ -5,44 +5,34 @@ module CabalScan.Options
   , parseCommandLine
   ) where
 
-import Data.List (intercalate)
 import Path (Abs, File, Path, parseAbsFile)
 import System.Environment
-import System.Exit
+import System.Console.GetOpt
+import System.Exit (exitFailure, exitSuccess)
+import Data.List (intercalate)
 import System.IO (hPutStrLn, stderr, stdout, Handle)
+
+data InputOptions = Help
+
+options :: [ OptDescr InputOptions ]
+options = [ Option ['h'] ["help"] (NoArg Help) "Prints this message" ]
 
 data Options = Options
   { cabalFiles :: [Path Abs File]
   }
 
-data InvocationError = MissingFiles | WrongFilePath String | PrintHelp
+parseArgs :: [String] -> IO [Path Abs File]
+parseArgs argv = case getOpt Permute options argv of
+                     (_:_,_,_)   -> hPutLn stdout [usage] *> exitSuccess
+                     (_,[],_)    -> hPutLn stderr [missingFiles, usage] *> exitFailure
+                     (_,files,_) -> sequence [ maybe (logErrAndExit f) return (parseAbsFile f) | f <- files ]
+  where usage = [usageInfo header options]
+        header = "Usage: cabalscan [OPTION...] CABAL_FILES..."
+        missingFiles = ["Missing: CABAL_FILES..."]
+        wrongpath f = ["Couldn't parse absolute file path: " ++ f]
+        logErrAndExit f = hPutLn stderr [wrongpath f, usage] *> exitFailure
+        hPutLn :: Handle -> [[String]] -> IO ()
+        hPutLn h = hPutStrLn h . intercalate "\n" . map unlines
 
 parseCommandLine :: IO Options
-parseCommandLine = do
-  args <- getArgs
-  case parseArgs args of
-    Right xs -> return (Options {cabalFiles = xs})
-    Left err -> printMsgAndExit err
-
-parseArgs :: [String] -> Either InvocationError [Path Abs File]
-parseArgs [] = Left MissingFiles
-parseArgs xs
-  | any (`elem` ["-h", "--help"]) xs = Left PrintHelp
-  | otherwise =
-    sequence [ maybe (Left $ WrongFilePath x) Right (parseAbsFile x) | x <- xs ]
-
-printMsgAndExit :: InvocationError -> IO a
-printMsgAndExit = \case
-  PrintHelp       -> hPutLn stdout [info, usage, options] *> exitSuccess
-  MissingFiles    -> hPutLn stderr [missing, usage]       *> exitFailure
-  WrongFilePath f -> hPutLn stderr [wrongpath f, usage]   *> exitFailure
-  where
-    info = ["cabalscan - extract build information from Cabal files"]
-    usage = ["Usage: cabalscan CABAL_FILES...",
-             "  Prints in stdout information extracted from Cabal files in JSON format."]
-    options = ["Available options:",
-               "-h,--help                Show this help text"]
-    missing = ["Missing: CABAL_FILES..."]
-    wrongpath p = ["Couldn't parse absolute file path: " ++ p]
-    hPutLn :: Handle -> [[String]] -> IO ()
-    hPutLn h = hPutStrLn h . intercalate "\n" . map unlines
+parseCommandLine = getArgs >>= parseArgs >>= return . Options
