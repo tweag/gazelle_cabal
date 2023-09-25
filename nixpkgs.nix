58558277e27dd3e415eefbd3496d0606651a9ebc
Andreas Herrmann <andreas.herrmann@tweag.io>
2023-09-25 20:07:15 +0200

    Update nixpkgs to include Bazel 6.3.2

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,1 +1,1 @@
-import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/refs/tags/23.05.tar.gz")
+import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/e35dcc04a3853da485a396bdd332217d0ac9054f.tar.gz")
